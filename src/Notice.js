21d110cca6a26031a78184b328a6f90ac1f9f4ea
liushzh <liushzh@yonyou.com>
2017-10-10 09:49:24 +0800

    react 16

--- src/Notice.js
+++ src/Notice.js
@@ -1,5 +1,6 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
 import classNames from 'classnames';
+import PropTypes from 'prop-types';
 
 const propTypes = {
     duration: PropTypes.number,
