b8e73a377cbe6cca25be12af09b6bc4f091b67a3
whynotgonow <whynotgonow@163.com>
2018-09-30 16:41:42 +0800

    添加callback校验

--- src/Notification.js
+++ src/Notification.js
@@ -105,6 +105,10 @@ Notification.propTypes = propTypes;
 Notification.defaultProps = defaultProps;
 
 Notification.newInstance = function newNotificationInstance(properties, callback) {
+  if (typeof callback !==  'function') {
+    throw 'You must introduce callback as the second parameter of Notification.newInstance().'
+    return
+  }
   const props = properties || {};
   const div = document.createElement('div');
   document.body.appendChild(div);
