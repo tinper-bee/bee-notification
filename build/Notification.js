c28fd60edb0b97a2b650cf375004382975ca6ca4
whynotgonow <whynotgonow@163.com>
2018-10-08 16:35:39 +0800

    publish 1.1.2

--- build/Notification.js
+++ build/Notification.js
@@ -166,7 +166,7 @@ Notification.defaultProps = defaultProps;
 
 Notification.newInstance = function newNotificationInstance(properties, callback) {
   if (typeof callback !== 'function') {
-    throw 'You must introduce callback as the second parameter of Notification.newInstance().';
+    console.error('You must introduce callback as the second parameter of Notification.newInstance().');
     return;
   }
   var props = properties || {};
